ccba4bab87ddcf4c09a2078017e0747136b06c3a
Erik Persson <loopen.madness@gmail.com>
2024-06-03 19:09:41 +0200

    removed files

--- pickle_reciever.py
+++
@@ -1,7 +0,0 @@
-import pickle
-import base64
-
-object = input("Pickled object= ")
-serial_object = base64.b64decode(object)
-deserial_object = pickle.loads(serial_object)
-print(dir(deserial_object))
